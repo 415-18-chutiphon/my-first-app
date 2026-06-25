d68ba01402318cfa54df75b4890eb4b1f5a55fb6
415-18-chutiphon <54751@yupparaj.ac.th>
2026-06-25 09:05:06 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("My Digital Profile")
+st.header("แนะนำตัว")
+st.write("นายชุติพนธ์ กุศล")
+st.write("ชื่อเล่นเคนโด้")
+st.write("เลขที่ 18 ม.4/15")
+st.header("อยากเล่นบาสโหดๆ")
+st.write("อยากเรียนบาส")
+st.write("เป็นนักบาส")
